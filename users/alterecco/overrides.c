0baceb33f5e0c0f0d8c27613bf6386a43a464452
alterecco <alterecco@slowmail.it>
2021-10-04 15:59:02 +0200

    enable overrides and combos

--- users/alterecco/overrides.c
+++ users/alterecco/overrides.c
@@ -2,12 +2,12 @@
 #include "keycodes.h"
 
 const key_override_t** key_overrides = (const key_override_t*[]){
-  &ko_make_basic(MOD_MASK_SHIFT,      _MINUS_,        _PLUS__),
-  &ko_make_basic(MOD_MASK_SHIFT,      _COMMA_,        _UNDRS_),
-  &ko_make_basic(MOD_MASK_SHIFT,      _GRAVE_,        _EQUAL_),
-  &ko_make_basic(MOD_MASK_SHIFT,      KC_DOT,         _AMPRS_),
-  &ko_make_basic(MOD_MASK_SHIFT,      KC_SLASH,       _ASTRS_),
-  &ko_make_basic(MOD_MASK_SHIFT,      _SQUOT_,        _EXCLM_),
-  &ko_make_basic(MOD_MASK_SHIFT,      _DQUOT_,        _QUEST_),
+  &ko_make_basic(MOD_MASK_SHIFT,      KC_MINUS,        KC_PLUS),
+  // &ko_make_basic(MOD_MASK_SHIFT,      _COMMA_,        _UNDRS_),
+  // &ko_make_basic(MOD_MASK_SHIFT,      _GRAVE_,        _EQUAL_),
+  // &ko_make_basic(MOD_MASK_SHIFT,      KC_DOT,         _AMPRS_),
+  // &ko_make_basic(MOD_MASK_SHIFT,      KC_SLASH,       _ASTRS_),
+  // &ko_make_basic(MOD_MASK_SHIFT,      _SQUOT_,        _EXCLM_),
+  // &ko_make_basic(MOD_MASK_SHIFT,      _DQUOT_,        _QUEST_),
   NULL
 };

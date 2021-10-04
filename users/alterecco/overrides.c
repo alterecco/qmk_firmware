5ebb32b08603b2e7f99758de5526112c276d7bfc
alterecco <alterecco@slowmail.it>
2021-10-04 16:06:48 +0200

    enable overrides and combos

--- users/alterecco/overrides.c
+++ users/alterecco/overrides.c
@@ -2,7 +2,7 @@
 #include "keycodes.h"
 
 const key_override_t** key_overrides = (const key_override_t*[]){
-  &ko_make_basic(MOD_MASK_SHIFT,      KC_MINUS,        KC_PLUS),
+  &ko_make_basic(MOD_MASK_SHIFT,      KC_MINUS,        LSFT(KC_EQUAL)),
   // &ko_make_basic(MOD_MASK_SHIFT,      _COMMA_,        _UNDRS_),
   // &ko_make_basic(MOD_MASK_SHIFT,      _GRAVE_,        _EQUAL_),
   // &ko_make_basic(MOD_MASK_SHIFT,      KC_DOT,         _AMPRS_),
